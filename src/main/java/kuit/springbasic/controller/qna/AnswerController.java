7118e8f383d9f6055bbfd3c84862f241dbee2401
kisusu <kikisu115@naver.com>
2024-11-12 20:03:56 +0900

    feat: Qna 관련 Controller 구현

--- src/main/java/kuit/springbasic/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/controller/qna/AnswerController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.SQLException;
@@ -41,7 +42,7 @@ public class AnswerController {
 
         Answer savedAnswer = answerRepository.insert(answer);
 
-        Question question = questionRepository.findByQuestionId(answer.getQuestionId());
+        Question question = questionRepository.findByQuestionId(savedAnswer.getQuestionId());
         question.increaseCountOfAnswer();
         questionRepository.update(question);
 
