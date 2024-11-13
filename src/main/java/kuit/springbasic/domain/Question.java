f0562419d1d12ddc80790d3435740f8e82272f68
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-11-13 14:19:10 +0900

    feat: 실습 강의

--- src/main/java/kuit/springbasic/domain/Question.java
+++ src/main/java/kuit/springbasic/domain/Question.java
@@ -1,9 +1,16 @@
 package kuit.springbasic.domain;
 
 
+import lombok.*;
+
 import java.sql.Date;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
     private int questionId;
     private String writer;
@@ -11,19 +18,6 @@ public class Question {
     private String contents;
     private Date createdDate;
     private int countOfAnswer;
-
-    public Question() {
-
-    }
-
-    public Question(int questionId, String writer, String title, String contents, Date createdDate, int countOfAnswer) {
-        this.questionId = questionId;
-        this.writer = writer;
-        this.title = title;
-        this.contents = contents;
-        this.createdDate = createdDate;
-        this.countOfAnswer = countOfAnswer;
-    }
 
     public Question(String writer, String title, String contents, int countOfAnswer) {
         this.questionId = 0;
@@ -33,55 +27,6 @@ public class Question {
         this.createdDate = Date.valueOf(LocalDate.now());
         this.countOfAnswer = countOfAnswer;
     }
-
-    public int getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(int questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public int getCountOfAnswer() {
-        return countOfAnswer;
-    }
-
-    public void setCountOfAnswer(int countOfAnswer) {
-        this.countOfAnswer = countOfAnswer;
-    }
-
     public void updateTitleAndContents(String title, String contents) {
         this.title = title;
         this.contents = contents;
@@ -108,17 +53,5 @@ public class Question {
         this.contents = question.contents;
         this.createdDate = question.createdDate;
         this.countOfAnswer = question.countOfAnswer;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "questionId=" + questionId +
-                ", writer='" + writer + '\'' +
-                ", title='" + title + '\'' +
-                ", contents='" + contents + '\'' +
-                ", createdDate=" + createdDate +
-                ", countOfAnswer=" + countOfAnswer +
-                '}';
     }
 }
