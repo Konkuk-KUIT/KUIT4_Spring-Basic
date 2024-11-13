76d8c7c2db5a147de0d2ec0ea76aed3cc1e9f1a9
juyeon <jjjjyy020704@naver.com>
2024-11-13 11:03:28 +0900

    HomeController,loginController 구현- 로그인 성공 시 redirect '/homeV1'로 변경

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -1,14 +1,39 @@
 package kuit.springbasic.controller;
 
-public class LoginController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import static kuit.springbasic.util.UserSessionUtils.USER_SESSION_KEY;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final UserRepository userRepository;
     /**
      * TODO: showLoginForm
      */
 
+    @RequestMapping("/user/loginForm")
+    public String showLoginForm() {
+        log.info("showLoginForm");
+        return "/user/login";
+    }
     /**
      * TODO: showLoginFailed
      */
+    @RequestMapping("/user/loginFailed")
+    public String showLoginFailed() {
+        log.info("showLoginFailed");
+        return "/user/loginFailed";
+    }
 
     /**
      * TODO: login
@@ -17,6 +42,37 @@ public class LoginController {
      * loginV3 : @RequestParam 생략(비추천)
      * loginV4 : @ModelAttribute
      */
+    //@RequestMapping("/user/login")
+    public String loginV1(@RequestParam("userId") String userId,
+                          @RequestParam("password") String password,
+                          HttpServletRequest request){
+        log.info("loginV1");
+        User loggedInUser = new User(userId, password);
+        User user = userRepository.findByUserId(userId);
+
+        if(user != null && user.isSameUser(loggedInUser)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", loggedInUser);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+
+    @RequestMapping("/user/login")
+    public String loginV2(@RequestParam("userId") String userId,
+                          @RequestParam("password") String password,
+                          HttpServletRequest request){
+        log.info("loginV2");
+        User loggedInUser = new User(userId, password);
+        User user = userRepository.findByUserId(userId);
+
+        if(user != null && user.isSameUser(loggedInUser)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", loggedInUser);
+            return "redirect:/homeV1";
+        }
+        return "redirect:/user/loginFailed";
+    }
 
     /**
      * TODO: logout
