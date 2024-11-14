f8210108d3ffb55b9dcdc86261729554e681def3
juyeon <jjjjyy020704@naver.com>
2024-11-14 17:24:33 +0900

    QuestionController 질문 생성까지 구현 - createQuestion 시 V1는 오류(V2는 노오류);이유를 못찾겟어

--- src/main/java/kuit/springbasic/controller/qna/QuestionController.java
+++ src/main/java/kuit/springbasic/controller/qna/QuestionController.java
@@ -2,12 +2,17 @@ package kuit.springbasic.controller.qna;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.db.QuestionRepository;
 import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.Question;
+import kuit.springbasic.domain.User;
 import kuit.springbasic.util.UserSessionUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class QuestionController {
 
     private final UserRepository userRepository;
+    private final QuestionRepository questionRepository;
     /**
      * TODO: showQuestionForm
      */
@@ -35,6 +41,28 @@ public class QuestionController {
      * createQuestionV1 : @RequestParam
      * createQuestionV2 : @ModelAttribute
      */
+    //@RequestMapping("/qna/create")
+    public String createQuestionV1(@RequestParam("writer") String writer,
+                                   @RequestParam("title") String title,
+                                   @RequestParam("contents") String contents,
+                                   @RequestParam("countOfAnswer")Integer countOfAnswer,
+                                   HttpServletRequest request){
+        log.info("createQuestion V1");
+        Question saveQuestion = new Question(writer, title, contents, countOfAnswer);
+
+        questionRepository.insert(saveQuestion);
+        return "redirect:/qna/show";
+    }
+    //V1은 생성이 안 됨 (whiteLabel)
+    //V2는 잘 됨
+    @RequestMapping("/qna/create")
+    public String createQuestionV2(@ModelAttribute Question saveQuestion,
+                                   HttpServletRequest request){
+        log.info("createQuestion V1");
+
+        questionRepository.insert(saveQuestion);
+        return "redirect:/qna/show";
+    }
 
     /**
      * TODO: showUpdateQuestionForm
@@ -46,5 +74,6 @@ public class QuestionController {
     /**
      * TODO: updateQuestion
      */
+
 
 }
