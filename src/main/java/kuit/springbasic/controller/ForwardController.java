0aeeb07ad379a81d3e31bf588fec7d841be826cf
yuna <yjeong1217@gmail.com>
2024-11-14 17:02:12 +0900

    요구사항 0: ForwardController 제외 구현

---
+++ src/main/java/kuit/springbasic/controller/ForwardController.java
@@ -0,0 +1,23 @@
+package kuit.springbasic.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class ForwardController {
+    private final String forwardUrl;
+
+    public ForwardController(String forwardUrl) {
+        this.forwardUrl = forwardUrl;
+        if (forwardUrl == null) {
+            throw new NullPointerException("forwardUrl is null");
+        }
+    }
+
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        return new ModelAndView(new JspView(forwardUrl));
+    }
+}
