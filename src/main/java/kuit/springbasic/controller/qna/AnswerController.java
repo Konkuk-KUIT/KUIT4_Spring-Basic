2ea29057c8085fee764821c2c5d325a549a804ae
yskim6772 <yskim6772@gmail.com>
2024-11-15 17:43:48 +0900

    [feat] : 7th mission

--- src/main/java/kuit/springbasic/controller/qna/AnswerController.java
+++ src/main/java/kuit/springbasic/controller/qna/AnswerController.java
@@ -1,6 +1,30 @@
 package kuit.springbasic.controller.qna;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.db.AnswerRepository;
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/qna")
 public class AnswerController {
+
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
 
     /**
      * TODO: addAnswer - @PostMapping
@@ -9,5 +33,73 @@ public class AnswerController {
      * addAnswerV2 : @RequestParam, @ResponseBody
      * addAnswerV3 : @ModelAttribute, @ResponseBody
      */
+    // @PostMapping("/addAnswer")
+    public void addAnswerV0(@RequestParam("questionId") int questionId,
+                            @RequestParam("writer") String writer,
+                            @RequestParam("contents") String contents,
+                            HttpServletResponse response) throws SQLException, IOException {
+        log.info("addAnswerV0");
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = answerRepository.insert(answer);
 
+        Question question = questionRepository.findByQuestionId(questionId);
+        question.increaseCountOfAnswer();
+        questionRepository.updateCountOfAnswer(question);
+
+        Map<String,Object> model = new HashMap<>();
+        model.put("answer", savedAnswer);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().print(new ObjectMapper().writeValueAsString(model));
+    }
+
+    // @PostMapping("/addAnswer")
+    public String addAnswerV1(@RequestParam("questionId") int questionId,
+                            @RequestParam("writer") String writer,
+                            @RequestParam("contents") String contents,
+                            Model model) {
+        log.info("addAnswerV1");
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = answerRepository.insert(answer);
+
+        Question question = questionRepository.findByQuestionId(questionId);
+        question.increaseCountOfAnswer();
+        questionRepository.updateCountOfAnswer(question);
+
+        model.addAttribute("answer", savedAnswer);
+
+        return "jsonView";
+    }
+
+    // @PostMapping("/addAnswer")
+    // @ResponseBody
+    public Map<String, Object> addAnswerV2(@RequestParam("questionId") int questionId,
+                                           @RequestParam("writer") String writer,
+                                           @RequestParam("contents") String contents) {
+        log.info("addAnswerV2");
+        Answer answer = new Answer(questionId, writer, contents);
+        Answer savedAnswer = answerRepository.insert(answer);
+
+        Question question = questionRepository.findByQuestionId(questionId);
+        question.increaseCountOfAnswer();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("answer", savedAnswer);
+
+        return response;
+    }
+
+    @PostMapping("/addAnswer")
+    @ResponseBody
+    public Answer addAnswerV3(@ModelAttribute Answer answer) throws SQLException {
+        log.info("addAnswerV3");
+        Answer savedAnswer = answerRepository.insert(answer);
+
+        Question question = questionRepository.findByQuestionId(answer.getQuestionId());
+        question.increaseCountOfAnswer();
+        questionRepository.updateCountOfAnswer(question);
+
+        return savedAnswer;
+    }
 }
--- src/main/java/kuit/springbasic/controller/qna/QnaController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnaController.java
@@ -1,9 +1,41 @@
 package kuit.springbasic.controller.qna;
 
+import kuit.springbasic.db.AnswerRepository;
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.domain.Answer;
+import kuit.springbasic.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/qna")
 public class QnaController {
+
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
 
     /**
      * TODO: showQnA
      */
+    @GetMapping("/show")
+    public ModelAndView showQnA(@RequestParam("questionId") int questionId) {
+        log.info("showQnA");
+
+        Question question = questionRepository.findByQuestionId(questionId);
+        Collection<Answer> answers = answerRepository.findAllByQuestionId(questionId);
+
+        return new ModelAndView("/qna/show")
+                .addObject("question", question)
+                .addObject("answers", answers);
+    }
 
 }
